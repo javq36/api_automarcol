1385db4ec944184d0fd9e5e4650589d6eef91620
SOLUDOCTUS\jorge.villamizar <jorge.villamizar@doctus.com.co>
2023-02-04 16:00:01 -0500

    Added format price for reports

--- src/controllers/peugeot.js
+++ src/controllers/peugeot.js
@@ -1,5 +1,5 @@
 import { getConection } from "../databases/conection";
-import { paginateAll } from "../helpers/functions";
+import { formatCOP, paginateAll } from "../helpers/functions";
 
 /* Method that search in all databases(Sales & Services) the client by plate. */
 export const getPeugeotInv = async (req, res) => {
@@ -39,13 +39,26 @@ from
     if (!!peugeot) {
       const filteredCars = peugeot.recordset.reduce((uniqueCars, car) => {
         const version = car.Version_DescipcionModelo.trim().toUpperCase();
-        if (car.CostoTotal >= 0 && !uniqueCars.some(uniqueCar => uniqueCar.Version_DescipcionModelo.trim().toUpperCase() === version)) {
-            car.Version_DescipcionModelo = version;
-            uniqueCars.push(car);
+        if (
+          car.CostoTotal >= 0 &&
+          !uniqueCars.some(
+            (uniqueCar) =>
+              uniqueCar.Version_DescipcionModelo.trim().toUpperCase() ===
+              version
+          )
+        ) {
+          car.Version_DescipcionModelo = version;
+          uniqueCars.push(car);
         }
         return uniqueCars;
-    }, []);
-      return res.status(200).json(filteredCars);
+      }, []);
+      const pricedCars = filteredCars.map((car) => {
+        return {
+          ...car,
+          CostoTotal: formatCOP(parseInt(car.CostoTotal)),
+        };
+      });
+      return res.status(200).json(pricedCars);
     }
     /*  if everything else fails, return a 404 error. */
     return res.status(404).json({ message: "operation failed" });
@@ -75,7 +88,14 @@ export const getPeugeotRep = async (req, res) => {
     LEFT JOIN img_modelo AS img ON img.modelo = I1.NumeroParte`);
 
     if (!!peugeot) {
-      let result = paginateAll(peugeot.recordset, 20)
+      const pricedReps = peugeot.recordset.map((rep) => {
+        return {
+          ...rep,
+          Costo$: formatCOP(parseInt(rep.Costo$)),
+        };
+      });
+
+      let result = paginateAll(pricedReps, 20);
       return res.status(200).json(result);
     }
     /*  if everything else fails, return a 404 error. */
