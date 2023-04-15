0a247a6f6a3112ec93875bb7765b0b9c1bef0f58
SOLUDOCTUS\jorge.villamizar <jorge.villamizar@doctus.com.co>
2023-04-15 09:16:31 -0500

    added new comment

--- src/databases/conection.js
+++ src/databases/conection.js
@@ -4,7 +4,7 @@ const dbSettings = {
   user: "Sistemas",
   password: "S1st3ma5+2023,",
   server: "190.85.51.38",
-  /* server: "192.168.1.91", */
+  /* server: "192.168.1.91", provisional */
   port: 1433,
   connectionTimeout: 30000,
   instance: "MSSQLSERVER",
