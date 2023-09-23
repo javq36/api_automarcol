bac4e325e112979b8d1e6ec8452490248a09718a
AUTOMARCOLSAS <139817714+AUTOMARCOLSAS@users.noreply.github.com>
2023-09-23 12:40:10 -0500

    Update clients.js

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getClients, getClientsById, getClientCarInfo , getDocumentsTerceros , getMantenimientos, getTall } from "../controllers/";
+import { getClients, getClientsById, getClientCarInfo , getDocumentsTerceros , getMantenimientos, getTall , getVts } from "../controllers/";
 import { validarCampos } from "../middlewares/";
 import { check } from "express-validator";
 
@@ -9,6 +9,7 @@ export const clientRoute = Router();
 clientRoute.get("/mantenimientos", getMantenimientos);
 clientRoute.post("/1105", getDocumentsTerceros);
 clientRoute.post("/getTall", getTall);
+clientRoute.post("/getVts", getVts);
 clientRoute.get("/", getClients);
 
 clientRoute.post(
