6383339000b37fe9aadb219baac3a1b01aa54530
Sebastian Vargaz <139817714+AUTOMARCOLSAS@users.noreply.github.com>
2026-07-17 20:05:33 -0500

    Update general.js

--- src/controllers/general.js
+++ src/controllers/general.js
@@ -6,59 +6,7 @@ export const getApiCartera = async (req, res) => {
   try {
     const CONQ = await pool
       .request()
-      .query(`
-          SELECT
-        a.Factura,
-        a.nit_Cedula,
-        a.NombresCliente,
-        a.Saldo as costo,
-        a.SaldoCorriente as saldo,
-        a.Saldo - a.SaldoCorriente as abonado,
-        b.CostoTotal,
-        b.Vin,
-        b.ventatotal,
-        b.Marca,
-        b.Version_DescripcionModelo,
-        b.Ano_modelo,
-        b.color,
-        FORMAT(b.fechafactura, 'yyyy-MM-dd') AS FechaFactura,
-        b.notas2 as comentarios,
-        d.notas as notas_compra,
-        DATEDIFF(day, b.fechafactura, GETDATE()) AS dias,
-        b.NombreAsesorComercial,
-        FORMAT(b.fechafactura, 'yyyy-MM') AS ym,
-        CASE
-            WHEN b.condicion = 1 THEN 'CREDITO'
-            ELSE 'CONTADO'
-        END AS condicion
-    FROM CARTERA_UNIDAS as a
-    LEFT JOIN API_VENTAS as b
-        ON b.Factura = a.Factura
-    LEFT JOIN documentos as o
-        ON o.tipo = LEFT(a.Factura, CHARINDEX('-', a.Factura) - 1)
-       AND o.numero = CAST(
-            RIGHT(
-                a.Factura,
-                LEN(a.Factura) - CHARINDEX('-', a.Factura)
-            ) AS INT
-        )
-    OUTER APPLY (
-        SELECT TOP 1 c.tipo, c.numero
-        FROM documentos_lin c
-        WHERE c.codigo = b.Vin
-          AND c.sw = 3
-        ORDER BY c.numero DESC
-    ) c
-    OUTER APPLY (
-        SELECT TOP 1 dd.notas
-        FROM documentos dd
-        WHERE dd.tipo = c.tipo
-          AND dd.numero = c.numero
-          AND dd.sw = 3
-    ) d
-    WHERE a.TipoCartera = 'Vehículos'
-      AND a.Factura != 'FVN-2484'
-      `);
+      .query(`SELECT * from API_CARTERA`);
     if (!!CONQ) {
       return res.status(200).json(CONQ.recordset);
     }
