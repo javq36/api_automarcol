1e532136a986e482c58dcf53b5dc9db96b0e8943
Sebastian Vargaz <139817714+AUTOMARCOLSAS@users.noreply.github.com>
2026-09-14 13:15:56 -0500

    Update clients.js

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getClients, getClientsById, getClientCarInfo , getDocumentsTerceros , getMantenimientos, getTall , getNuevos, getimotriz , getMostradorEncuestas , getRecibosCaja, getRecibosCaja_U, getRecibosCaja_C, addRc, getDistribuidor } from "../controllers/";
+import { getClients, getClientsById, getClientCarInfo , getDocumentsTerceros , getMantenimientos, getTall , getNuevos, getimotriz , getMostradorEncuestas , getRecibosCaja, getRecibosCaja_U, getRecibosCaja_C, addRc, getDistribuidor, updateUbicacion  } from "../controllers/";
 import { validarCampos } from "../middlewares/";
 import { check } from "express-validator";
 
 export const clientRoute = Router();
 clientRoute.post("/getRecibosCaja_U", getRecibosCaja_U);
 clientRoute.post("/addRc", addRc);
+clientRoute.post("/updateUbicacion", updateUbicacion);
 clientRoute.post("/getRecibosCaja_C", getRecibosCaja_C);
 clientRoute.post("/getRecibosCaja", getRecibosCaja);
 clientRoute.get("/imotriz", getimotriz);
